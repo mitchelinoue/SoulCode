5d81cf44ec29045aa17dec282d796d22463ede5e
mitchelinoue <mitchel_inoue@msn.com>
2022-10-11 17:41:18 -0300

    início projeto angular

--- ExerciciosJava/src/main/java/Ex1.java
+++ ExerciciosJava/src/main/java/Ex1.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
 public class Ex1 {
-    publi
+    public static void main(String[] args){
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.println("Qual a quantidade mínima do produto?");
+    int quantidadeMin = entrada.nextInt();
+
+    System.out.println("Qual a quantidade máxima do produto?");
+    int quantidadeMax = entrada.nextInt();
+
+    double media = (quantidadeMin + quantidadeMax) / 2;
+
+    System.out.println("O estoque médio é de: " + media);
+
+
+
+    }
 }
